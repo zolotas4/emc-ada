0ac9f6108c9cd80354e430f7e9acc70d6b95d9d8
Athanasios Zolotas <zolotas4@gmail.com>
2016-11-10 17:00:36 +0000

    Configuration dialog and icon

--- org.eclipse.epsilon.emc.ada.dt/src/org/eclipse/epsilon/emc/ada/dt/AdaModelConfigurationDialog.java
+++ org.eclipse.epsilon.emc.ada.dt/src/org/eclipse/epsilon/emc/ada/dt/AdaModelConfigurationDialog.java
@@ -1,17 +1,47 @@
 package org.eclipse.epsilon.emc.ada.dt;
 
 import org.eclipse.epsilon.common.dt.launching.dialogs.AbstractCachedModelConfigurationDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 public class AdaModelConfigurationDialog extends AbstractCachedModelConfigurationDialog {
 
+	private Text fileText;
+	
 	@Override
 	protected String getModelName() {
-		return null;
+		return "Ada Model";
 	}
 
 	@Override
 	protected String getModelType() {
-		return null;
+		return "Ada";
+	}
+	
+	@Override
+	protected void createGroups(Composite control) {
+		super.createGroups(control);
+		createFileGroup(control);
+		createLoadStoreOptionsGroup(control);
+	}
+
+	private void createFileGroup(Composite parent) {
+		final Composite groupContent = createGroupContainer(parent, "Files", 3);
+		
+		final Label modelFileLabel = new Label(groupContent, SWT.NONE);
+		modelFileLabel.setText("Model file: ");
+		
+		fileText = new Text(groupContent, SWT.BORDER);
+		fileText.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		final Button browseFile = new Button(groupContent, SWT.NONE);
+		browseFile.setText("Browse Workspace...");
+		browseFile.addListener(SWT.Selection, new BrowseWorkspaceForModelsListener(fileText, "Ada files in the workspace", "Select an Ada file"));
+		
 	}
 
 }
