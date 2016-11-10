81117053f0f1efca21c891196417ae54546ba436
Athanasios Zolotas <zolotas4@gmail.com>
2016-11-10 15:57:32 +0000

    Configuration dialog creation and auto methods generation

---
+++ org.eclipse.epsilon.emc.ada.dt/src/org/eclipse/epsilon/emc/ada/dt/AdaModelConfigurationDialog.java
@@ -0,0 +1,17 @@
+package org.eclipse.epsilon.emc.ada.dt;
+
+import org.eclipse.epsilon.common.dt.launching.dialogs.AbstractCachedModelConfigurationDialog;
+
+public class AdaModelConfigurationDialog extends AbstractCachedModelConfigurationDialog {
+
+	@Override
+	protected String getModelName() {
+		return null;
+	}
+
+	@Override
+	protected String getModelType() {
+		return null;
+	}
+
+}
